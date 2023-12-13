9596fd8dae7927138543064bb70e9df059b35e02
KiyoLelou10 <asumshik@gmail.com>
2023-12-13 20:08:13 +0100

    new time function

--- src/Test/test1.java
+++ src/Test/test1.java
@@ -11,6 +11,7 @@ public class test1 {
 	public static void main(String[] args) {
 		DroneBuilder x = new DroneBuilder();
 		Count.printlist();
+		//tests time function
 		DroneTime y = new DroneTime("2023-12-01T15:43:27.940361+01:00");
 		System.out.println(y);
 		
