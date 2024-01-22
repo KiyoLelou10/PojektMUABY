d40110ad4041f65f40e95341120daf90cbb56195
Mohit Kumawat <115658001+mo-heatt@users.noreply.github.com>
2024-01-22 14:58:38 +0100

    frontend clean code

--- src/frontend/Loading_Screen.java
+++ src/frontend/Loading_Screen.java
@@ -1,4 +1,3 @@
-
 package frontend;
 
 import java.awt.BorderLayout;
@@ -16,7 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Loading_Screen extends JFrame {
-	 protected JFrame frame;
+	protected JFrame frame;
 	 
 	public Loading_Screen() {
 		JFrame frame = initialize();
@@ -34,7 +33,6 @@ public class Loading_Screen extends JFrame {
         frame.setLayout(new FlowLayout());
         frame.pack();
         centerFrameOnScreen(frame);
- 
     }
 	
 	protected JFrame initialize() {
@@ -45,12 +43,10 @@ public class Loading_Screen extends JFrame {
 		frame.setVisible(true);
 		
 		return frame;
-		
-		
 	}
+
 	@Override
 	public void dispose() {
-		
 		frame.dispose();
 	}
 	
@@ -61,135 +57,3 @@ public class Loading_Screen extends JFrame {
         frame.setLocation(centerX, centerY);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-/*package frontend;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Image;
-import java.awt.Toolkit;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class Loading_Screen extends JFrame {
-	
-	
-	
-	protected JFrame frame;
-	
-	public Loading_Screen() {
-		JFrame frame=initialize(); 
-		
-		JLabel frontLabel= JlabelwithTextAndImage("Welcome to the World of Drones, Would you like to explore this Universe??", "images/loading gif.gif");
-		frontLabel.setBounds(10,10,1000,1000);
-		frame.add(frontLabel, BorderLayout.CENTER);
-		frame.pack();
-	}
-	protected JFrame initialize() {
-		frame = new JFrame();
-		
-		
-		 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	        int width = (int)(screenSize.getWidth());
-	        int height = (int)(screenSize.getHeight());
-	          
-	    frame.getContentPane().setEnabled(false);
-		frame.setSize(width, height);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
-		//frame.setExtendedState(MAXIMIZED_BOTH);
-		frame.setVisible(true);
-		
-		return frame;
-		
-		
-	}
-	
-	
-	private JLabel JlabelwithTextAndImage(String text,String imgPath) {
-		JLabel a= new JLabel();
-		a.setText(text);
-		//adding image 
-		ImageIcon img= new ImageIcon(imgPath);
-		Image resizedImage= img.getImage().getScaledInstance(400, 400,Image.SCALE_SMOOTH);
-		a.setIcon(new ImageIcon(resizedImage));
-		
-		
-		//Alignment of image and text 
-		a.setVerticalAlignment(JLabel.TOP);
-		a.setHorizontalAlignment(JLabel.CENTER);
-		a.setHorizontalTextPosition(JLabel.CENTER);
-		a.setVerticalTextPosition(JLabel.BOTTOM);
-		
-		a.setFont(new Font("Segoe UI", Font.BOLD, 30));
-		a.setForeground(Color.GRAY);
-		return a;
-	}
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	protected JFrame frame;
-	protected JLabel text;
-	protected JLabel label;
-	
-	public Loading_Screen() {
-		 
-		JFrame frame=initialize();
-		text = new JLabel("Loading the data..."); 
-		frame.add(text);
-		
-	}
-
-	protected JFrame initialize() {
-		frame = new JFrame();
-		
-		
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	    int width = (int)screenSize.getWidth();
-	    int height = (int)screenSize.getHeight();
-	          
-	    frame.getContentPane().setEnabled(false);
-		frame.setSize(width, height);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
-		frame.setVisible(true);
-		
-		return frame;
-		
-		
-	}
-	
-	
-	@Override
-	public void dispose() {
-		
-		frame.dispose();
-	}
-}
-*/
